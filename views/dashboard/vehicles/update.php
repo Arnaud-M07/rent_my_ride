3d58a95bbf6f90651c2f72139d1d6c168fbae276
Arnaud-M07 <arnaudmonteux@gmail.com>
2024-01-15 12:14:04 +0100

    $category->name dans vehicles > update.php ok

--- views/dashboard/vehicles/update.php
+++ views/dashboard/vehicles/update.php
@@ -15,28 +15,14 @@
                 <!-- vehicleBrand -->
                 <div class="col col-md-6">
                     <label for="vehicleBrand" class="form-label">Marque du véhicule :</label>
-                    <input pattern="<?= REGEX_CATEGORY ?>" 
-                    value="<?= $vehicle->brand ?? '' ?>" 
-                    name="vehicleBrand" 
-                    type="text" 
-                    class="form-control" 
-                    placeholder="<?= $vehicle->brand ?? $vehicleBrand ?>" 
-                    id="vehicleBrand" 
-                    required>
+                    <input pattern="<?= REGEX_CATEGORY ?>" value="<?= $vehicle->brand ?? '' ?>" name="vehicleBrand" type="text" class="form-control" placeholder="<?= $vehicle->brand ?? $vehicleBrand ?>" id="vehicleBrand" required>
                     <small class="alert-message"><?= $error['vehicleBrand'] ?? '' ?></small>
                     <small class="addToBdd-message"><?= $addedToDb['vehicleBrand'] ?? '' ?></small>
                 </div>
                 <!-- vehicleModel -->
                 <div class="col col-md-6">
                     <label for="vehicleModel" class="form-label">Modèle du véhicule :</label>
-                    <input pattern="<?= REGEX_CATEGORY ?>" 
-                    value="<?= $vehicle->model ?? '' ?>" 
-                    name="vehicleModel" 
-                    type="text" 
-                    class="form-control" 
-                    placeholder="<?= $vehicle->model ?? $vehicleModel ?>" 
-                    id="vehicleModel" 
-                    required>
+                    <input pattern="<?= REGEX_CATEGORY ?>" value="<?= $vehicle->model ?? '' ?>" name="vehicleModel" type="text" class="form-control" placeholder="<?= $vehicle->model ?? $vehicleModel ?>" id="vehicleModel" required>
                     <small class="alert-message"><?= $error['vehicleModel'] ?? '' ?></small>
                     <small class="addToBdd-message"><?= $addedToDb['vehicleModel'] ?? '' ?></small>
                 </div>
@@ -45,28 +31,14 @@
                 <!-- vehicleRegistration -->
                 <div class="col col-md-6">
                     <label for="vehicleRegistration" class="form-label">Immatriculation du véhicule :</label>
-                    <input pattern="<?= REGEX_REGISTRATION ?>" 
-                    value="<?= $vehicle->registration ?? '' ?>" 
-                    name="vehicleRegistration" 
-                    type="text" 
-                    class="form-control" 
-                    placeholder="<?= $vehicle->registration ?? $vehicleRegistration ?>" 
-                    id="vehicleRegistration" 
-                    required>
+                    <input pattern="<?= REGEX_REGISTRATION ?>" value="<?= $vehicle->registration ?? '' ?>" name="vehicleRegistration" type="text" class="form-control" placeholder="<?= $vehicle->registration ?? $vehicleRegistration ?>" id="vehicleRegistration" required>
                     <small class="alert-message"><?= $error['vehicleRegistration'] ?? '' ?></small>
                     <small class="addToBdd-message"><?= $addedToDb['vehicleRegistration'] ?? '' ?></small>
                 </div>
                 <!-- vehicleMileage -->
                 <div class="col col-md-6">
                     <label for="vehicleMileage" class="form-label">Kilométrage du véhicule :</label>
-                    <input pattern="<?= REGEX_MILEAGE ?>" 
-                    value="<?= $vehicle->mileage ?? '' ?>" 
-                    name="vehicleMileage" 
-                    type="number" 
-                    class="form-control" 
-                    placeholder="<?= $vehicle->mileage ?? $vehicleMileage ?>" 
-                    id="vehicleMileage" 
-                    required>
+                    <input pattern="<?= REGEX_MILEAGE ?>" value="<?= $vehicle->mileage ?? '' ?>" name="vehicleMileage" type="number" class="form-control" placeholder="<?= $vehicle->mileage ?? $vehicleMileage ?>" id="vehicleMileage" required>
                     <small class="alert-message"><?= $error['vehicleMileage'] ?? '' ?></small>
                     <small class="addToBdd-message"><?= $addedToDb['vehicleMileage'] ?? '' ?></small>
                 </div>
@@ -79,16 +51,13 @@
                     name="vehicleCategory" 
                     id="vehicleCategory" 
                     aria-label="Floating label select example">
-                        <option value="<?= $vehicle->id_category ?? '' ?>" 
-                        class="listNames" 
-                        disabled 
-                        selected 
-                        hidden><?=$category->name ?? 'séléctionnez une catégorie' ?></option>
-                        <?php foreach ($categories as $category) {
-                            $isSelected = ($category->name && $category->name == $categories) ? 'selected' : '';
-                            echo "<option value=\"$category->id_category\" $isSelected>" . ucfirst(strtolower($category->name)) . "</option>";
-                        }
-                        ?>
+                        <?php foreach ($categories as $category) { 
+                            $isSelected = ($category->id_category == $vehicle->id_category) ? 'selected' : '';
+                            ?>
+                            <option class="listNames" value="<?= $category->id_category ?>" <?= $isSelected ?>>
+                                <?= ucfirst(strtolower($category->name)) ?>
+                            </option>
+                        <?php } ?>
                     </select>
                     <small class="alert-message"><?= $error['vehicleCategory'] ?? '' ?></small>
                     <small class="addToBdd-message"><?= $addedToDb['vehicleCategory'] ?? '' ?></small>
@@ -96,11 +65,7 @@
                 <!-- vehiclePicture -->
                 <div class="col col-md-6">
                     <label for="vehiclePicture" class="form-label mb-0">Photo du véhicule</label>
-                    <input class="form-control" 
-                    name="vehiclePicture" 
-                    id="vehiclePicture" 
-                    type="file" 
-                    accept=".png, image/jpeg">
+                    <input class="form-control" name="vehiclePicture" id="vehiclePicture" type="file" accept=".png, image/jpeg">
                     <small class="alert-message"><?= $error['vehiclePicture'] ?? '' ?></small>
                     <small class="addToBdd-message"><?= $addedToDb['vehiclePicture'] ?? '' ?></small>
                 </div>
